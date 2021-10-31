d99fb912500745035b8bb4ccddb594c6624d7a20
Nando <nandodavila94@gmail.com>
2021-10-31 16:03:21 -0400

    finished classes, working on promts now

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
-const writeHTML = require('./lib/generateHTML')
+const Employee = require('./lib/employee')
+const Manager = require('./lib/manager')
+const Intern = require('./lib/intern')
+const Engineer = require('./lib/engineer')
+
+const employeeArray = [];
+
+const commonQuestions = [
+    {
+        type: 'checkbox',
+        message: 'What is this employee role?',
+        name: 'role'
+    },
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'What is your id?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email'
+    }
+];
+
+const managerQuestion = [
+    {
+        type: 'input',
+        message: 'What is your phone number',
+        name: 'phone'
+    },
+    
+];
+
+const engineerQuestion = [
+    {
+        type: 'input',
+        message: 'What is your github?',
+        name: 'github'
+    },
+    
+];
+
+const internQuestion = [
+    {
+        type: 'input',
+        message: 'Where do you go to school?',
+        name: 'school'
+    },
+    
+];
+
+
